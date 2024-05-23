7b128f47dc814e52a9f8fa05bbbf2b1c368a16f5
SARAH VITÓRIA DOS SANTOS GONÇALVES <232.933598@senaicimatec.edu.br>
2024-05-23 16:29:46 -0300

    Initial commit

---
+++ app/src/main/java/com/example/attvdvitoria1605/CadastroSarah2.java
@@ -0,0 +1,14 @@
+package com.example.attvdvitoria1605;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class CadastroSarah2 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_cadastro_sarah2);
+    }
+}
---
+++ app/src/main/java/com/example/attvdvitoria1605/MainActivity_cadastro_sarah3.java
@@ -0,0 +1,14 @@
+package com.example.attvdvitoria1605;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity_cadastro_sarah3 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main_cadastro_sarah3);
+    }
+}
